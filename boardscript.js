51ff9e172a3a876b6a58eef9d96feb766c09a709
damnge <dam.grela@gmail.com>
2022-10-20 01:07:50 +0000

    meditation is ready

--- boardscript.js
+++ boardscript.js
@@ -107,7 +107,7 @@ function displayGretting() {
     grettingMsg.innerHTML = `Good evening ${mainUsername}!`;
   }
 }
-
+// Swticher for left widgets
 switchBtn.addEventListener("click", () => {
   console.log(switchBtn.checked);
   if (switchBtn.checked) {
@@ -119,6 +119,90 @@ switchBtn.addEventListener("click", () => {
 displayGretting();
 setInterval(displayGretting, 60000);
 
+icons[1].addEventListener("click", () => {
+  document.getElementById("clock").style.display = "none";
+  document.getElementById("meditation").style.display = "flex";
+});
+
+icons[0].addEventListener("click", () => {
+  document.getElementById("clock").style.display = "flex";
+  document.getElementById("meditation").style.display = "none";
+});
 // icons[0].addEventListener("mouseover", () => {
 //   iconsInfo[0].style.display = "block";
 // });
+
+// ----- MEDITATION ------
+// Meditation app built with vanilla javascript
+const song = document.querySelector(".song");
+const play = document.querySelector(".play");
+const replay = document.querySelector(".replay");
+
+//Sounds
+const sounds = document.querySelectorAll(".song");
+//Time Display
+const timeDisplay = document.querySelector(".time-display");
+// Get the length of the outline for the time ring
+//Duration  for the meditations
+const timeSelect = document.querySelectorAll(".time__btn");
+let fakeDuration = 600;
+
+timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+  // minutes since the song started
+  fakeDuration % 60
+)}`;
+
+// play sound
+play.addEventListener("click", function () {
+  // add an event listener to the play button
+  checkPlaying(song); // call the checkPlaying function
+});
+
+// Select the duration of the meditation
+const restartSong = (song) => {
+  // call the restartSong function
+  let currentTime = song.currentTime; // get the current time
+  console.log(currentTime);
+  song.currentTime = 0; // set the current time to 0
+};
+
+timeSelect.forEach((option) => {
+  // select option from list of options
+  option.addEventListener("click", function () {
+    restartSong(song);
+    // add an event listener to the list of options selected
+    fakeDuration = this.getAttribute("data-time"); // get the data time attribute from each options selected
+    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+      // minutes since the song started
+      fakeDuration % 60
+    )}`;
+  });
+});
+// Create a function that will be called when the play button is clicked and will check if the song is playing or not
+const checkPlaying = (song) => {
+  // pass the song as a parameter
+  if (song.paused) {
+    // if the song is paused
+    document.getElementById("start-btn").textContent = "Pause";
+    song.play(); // play the song if it is playing and if it is not playing then stop playing
+  } else {
+    document.getElementById("start-btn").textContent = "Start";
+    song.pause();
+  }
+};
+// We can animate the circle
+song.ontimeupdate = function () {
+  // add an event listener to the song
+  let currentTime = song.currentTime; // get the current time of the song
+  let elapsed = fakeDuration - currentTime; // get the elapsed time of the song
+  let seconds = Math.floor(elapsed % 60); // seconds since the song started
+  let minutes = Math.floor(elapsed / 60); // minutes since the song started
+  timeDisplay.textContent = `${minutes}:${seconds}`; // display the minutes and seconds
+
+  if (currentTime >= fakeDuration) {
+    // if the current time is greater than or equal to the fake duration
+    song.pause(); // pause the song
+    song.currentTime = 0; // set the current time to 0
+    // change the play button to a  new path to play the song from the beginning
+  }
+};
