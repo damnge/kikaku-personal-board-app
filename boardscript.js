475ac5632d2852ae3776e59ac6059971b2faeb2b
damnge <dam.grela@gmail.com>
2022-10-20 12:13:51 +0000

    pomodoro connected

--- boardscript.js
+++ boardscript.js
@@ -23,15 +23,26 @@ let currentDate = "";
 icons[0].addEventListener("click", () => {
   document.getElementById("clock").style.display = "flex";
   document.getElementById("meditation").style.display = "none";
+  document.getElementById("pomodoro-timer").style.display = "none";
   song.pause();
   document.getElementById("start-btn").textContent = "Start";
 });
 
 icons[1].addEventListener("click", () => {
   document.getElementById("clock").style.display = "none";
+  document.getElementById("pomodoro-timer").style.display = "none";
   document.getElementById("meditation").style.display = "flex";
   document.getElementById("my-widgets").hidden = true;
   document.getElementById("widget-swticher").checked = false;
+});
+
+icons[2].addEventListener("click", () => {
+  document.getElementById("clock").style.display = "none";
+  document.getElementById("meditation").style.display = "none";
+  document.getElementById("pomodoro-timer").style.display = "flex";
+  document.getElementById("my-widgets").hidden = true;
+  document.getElementById("widget-swticher").checked = false;
+  song.pause();
 });
 
 // api Key for unslpash
@@ -115,7 +126,7 @@ function getCurrentTimeAndDate() {
   );
   myTime = date.toLocaleTimeString("en-GB", { timeStyle: "short" }).split(":");
 }
-getCurrentTimeAndDate();
+
 setInterval(getCurrentTimeAndDate, 1000);
 
 // WEATHER WIDGET --- fetching weather API
@@ -151,7 +162,6 @@ function displayGretting() {
   }
 }
 
-displayGretting();
 setInterval(displayGretting, 60000);
 
 // ----- MEDITATION APP ------
@@ -227,3 +237,167 @@ song.ontimeupdate = function () {
     // change the play button to a  new path to play the song from the beginning
   }
 };
+
+// POMODORO APP
+
+// Start by creating a timer variable with the following properties
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
+  sessions: 0,
+};
+// Add the ability to start the timer and countdown to zero.
+let interval;
+//
+const buttonSound = new Audio("button-sound.mp3");
+const mainButton = document.getElementById("js-btn");
+mainButton.addEventListener("click", () => {
+  buttonSound.play();
+  const { action } = mainButton.dataset;
+  if (action === "start") {
+    startTimer();
+  } else {
+    stopTimer();
+  }
+});
+// The next thing we need to do is update the countdown with the appropriate amount of minutes and seconds once any of the three buttons above it is clicked. To do this we need to create an event listener that detects a click on the buttons and a function to switch the mode of the timer appropriately.
+const modeButtons = document.querySelector("#js-mode-buttons");
+modeButtons.addEventListener("click", handleMode);
+// This function takes a timestamp argument and finds the difference between the current time and the end time in milliseconds. This value is stored in the difference variable and used to compute the total number of seconds left by dividing by 1000. The result is subsequently converted to an integer in base 10 through the Number.parseInt() method and stored in the total variable.
+function getRemainingTime(endTime) {
+  const currentTime = Date.parse(new Date());
+  const difference = endTime - currentTime;
+
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
+
+  return {
+    total,
+    minutes,
+    seconds,
+  };
+}
+//  The switchMode function is called with the current mode as an argument. The switchMode function then updates the timer object with the current mode and the remaining time for that mode. The updateClock function is then called to update the UI with the current mode and remaining time.
+function startTimer() {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
+
+  if (timer.mode === "pomodoro") timer.sessions++;
+
+  mainButton.dataset.action = "stop";
+  mainButton.textContent = "Stop";
+  mainButton.classList.add("active");
+
+  interval = setInterval(function () {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
+
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
+
+      switch (timer.mode) {
+        case "pomodoro":
+          if (timer.sessions % timer.longBreakInterval === 0) {
+            switchMode("longBreak");
+          } else {
+            switchMode("shortBreak");
+          }
+          break;
+        default:
+          switchMode("pomodoro");
+      }
+
+      if (Notification.permission === "granted") {
+        const text =
+          timer.mode === "pomodoro" ? "Get back to work!" : "Take a break!";
+        new Notification(text);
+      }
+
+      document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
+      startTimer();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+
+  mainButton.dataset.action = "start";
+  mainButton.textContent = "Start";
+  mainButton.classList.remove("active");
+}
+// updateClock() function is invoked. This function is how the countdown portion of the application is updated. It first updates the text of the timer to reflect the current time remaining. Next, it updates the progress bar to reflect the current time remaining. Finally, it updates the title of the page to reflect the current time remaining.
+function updateClock() {
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, "0");
+  const seconds = `${remainingTime.seconds}`.padStart(2, "0");
+
+  const min = document.getElementById("js-minutes");
+  const sec = document.getElementById("js-seconds");
+  min.textContent = minutes;
+  sec.textContent = seconds;
+
+  const text =
+    timer.mode === "pomodoro" ? "Get back to work!" : "Take a break!";
+  document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById("js-progress");
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
+}
+// The switchMode() function adds two new properties to the timer object. First, a mode property is set to the current mode which could be pomodoro, shortBreak or longBreak. Next, a remainingTime property is set on the timer.
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
+
+  document
+    .querySelectorAll("button[data-mode]")
+    .forEach((e) => e.classList.remove("active"));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
+  document.body.style.backgroundColor = `var(--${mode})`;
+  document
+    .getElementById("js-progress")
+    .setAttribute("max", timer.remainingTime.total);
+
+  updateClock();
+}
+
+function handleMode(event) {
+  const { mode } = event.target.dataset;
+
+  if (!mode) return;
+
+  switchMode(mode);
+  stopTimer();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if ("Notification" in window) {
+    if (
+      Notification.permission !== "granted" &&
+      Notification.permission !== "denied"
+    ) {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          new Notification(
+            "Awesome! You will be notified at the start of each session"
+          );
+        }
+      });
+    }
+  }
+
+  switchMode("pomodoro");
+});
+
+// On Load
+getCurrentTimeAndDate();
+displayGretting();
